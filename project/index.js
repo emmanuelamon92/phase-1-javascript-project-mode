14f09263da001c2498b61e14b0f8d526e4dbaaab
Emmanuel <emmanuelamon92@gmail.com>
2021-05-20 18:20:19 -0400

    added functions and event listeners

--- project/index.js
+++ project/index.js
@@ -1,13 +1,33 @@
-const url2 = "https://animechan.vercel.app/api/quotes/anime?title=naruto"
+const animeListUrl = "https://animechan.vercel.app/api/quotes/anime?title=naruto"
+const randomUrl = "https://animechan.vercel.app/api/random"
+const animeSearchButton = document.querySelector("#animeSearchButton")
+const userInput = document.querySelector('#user-input')
+const randomQuoteButton = document.querySelector('#randomQuoteButton')
 
-fetch (url2)
-    .then(response => response.json())
-	.then (animeArr => {
-		// console.log(animeArr)
-		for (const animeObjList of animeArr) {
-			const {anime, character, quote} = animeObjList;
-			// console.log(anime)
-			// console.log(character)
-			// console.log(quote) cl
-		}
+//EVENT LISTENERS
+//event listener for clicking submit button for form
+animeSearchButton.addEventListener('click', () => {
+	fetch (animeListUrl)
+    	.then(response => response.json())
+		.then (animeListArr => nameCharacterSearch(animeListArr))
 })
+
+//event listener for clicking random quote
+randomQuoteButton.addEventListener('click', () => {
+	fetch (randomUrl)
+		.then(response => response.json())
+		.then(randomArr => randomQuoteSearch(randomArr))
+})
+
+
+//FUNCTIONS
+//funtion for searching by name and character (seperate?)
+let nameCharacterSearch = animeListArr => {
+	for (const animeObjList of animeListArr) {
+		const {anime, character, quote} = animeObjList;
+	}
+}
+//function for searching random
+let randomQuoteSearch = randomArr => {
+	// console.log(randomArr)
+}
---
+++ project/random.js
@@ -0,0 +1,38 @@
+//RANDOM STUFF!!!
+
+// const url1 = "https://animechan.vercel.app/api/quotes/anime?title=naruto"
+// const url2 = "https://animechan.vercel.app/api/random"
+// const url3 = "https://animechan.vercel.app/api/quotes"
+// const url4 = "https://animechan.vercel.app/api/quotes/character?name=saitama"
+
+
+// fetch (url1)
+//     .then(response => response.json())
+// 	.then (quotes => console.log(quotes))
+// fetch (url2)
+//     .then(response => response.json())
+// 	.then (quotes => console.log(quotes))
+// fetch (url3)
+//     .then(response => response.json())
+// 	.then (quotes => console.log(quotes))
+// fetch (url4)
+//     .then(response => response.json())
+// 	.then (quotes => console.log(quotes))
+
+// fetch(url, {
+// 	method: 'GET', // *GET, POST, PUT, DELETE, etc.
+// 	mode: 'no-cors', // no-cors, *cors, same-origin
+// 	cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+// 	credentials: 'same-origin', // include, *same-origin, omit
+// 	})
+// 	.then(response => response.json())
+// 	.then (animeArr => console.log(animeArr))	
+// 	})
+
+
+
+// search with name
+// file is downloaded
+// search txt file
+// use json text file
+  
