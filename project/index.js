cd09bbe23dbdd1dd41e286467c2b320ec126c164
Emmanuel <emmanuelamon92@gmail.com>
2021-05-20 23:12:12 -0400

    more function manipulation

--- project/index.js
+++ project/index.js
@@ -3,31 +3,44 @@ const randomUrl = "https://animechan.vercel.app/api/random"
 const animeSearchButton = document.querySelector("#animeSearchButton")
 const userInput = document.querySelector('#user-input')
 const randomQuoteButton = document.querySelector('#randomQuoteButton')
+//................
 
 //EVENT LISTENERS
-//event listener for clicking submit button for form
-animeSearchButton.addEventListener('click', () => {
-	fetch (animeListUrl)
-    	.then(response => response.json())
-		.then (animeListArr => nameCharacterSearch(animeListArr))
+
+//Event listener for clicking submit button for form
+animeSearchButton.addEventListener('click', e => {
+	e.preventDefault();
+	return userQuoteFetch;
 })
 
-//event listener for clicking random quote
-randomQuoteButton.addEventListener('click', () => {
-	fetch (randomUrl)
-		.then(response => response.json())
-		.then(randomArr => randomQuoteSearch(randomArr))
-})
-
+//Event listener for clicking random quote
+randomQuoteButton.addEventListener('click', () => randomQuoteFetch)
+//.................
 
 //FUNCTIONS
-//funtion for searching by name and character (seperate?)
-let nameCharacterSearch = animeListArr => {
-	for (const animeObjList of animeListArr) {
-		const {anime, character, quote} = animeObjList;
-	}
+
+//Function to handle fetch
+let userQuoteFetch = () => {
+	return fetch (animeListUrl)
+    	.then(response => response.json())
+		.then (animeListArr => nameCharacterSearch(animeListArr))
 }
-//function for searching random
+
+let randomQuoteFetch = () => {
+	return fetch (randomUrl)
+		.then(response => response.json())
+		.then(randomArr => randomQuoteSearch(randomArr))
+}
+
+//Funtion for searching by name and character (seperate?)
+let nameCharacterSearch = animeListArr => {
+	console.log(animeListArr)
+	// for (const animeObjList of animeListArr) {
+	// 	const {anime, character, quote} = animeObjList;
+	// }
+}
+
+//Function for searching random
 let randomQuoteSearch = randomArr => {
-	// console.log(randomArr)
+	console.log(randomArr)
 }
