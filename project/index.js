7bd4e58009b7367e859677a0af90660173d52705
Emmanuel <emmanuelamon92@gmail.com>
2021-05-21 01:08:53 -0400

    add a couple of features to nameCharacterSearch function

--- project/index.js
+++ project/index.js
@@ -3,11 +3,10 @@ const randomUrl = "https://animechan.vercel.app/api/random"
 const animeSearchButton = document.querySelector("#animeSearchButton")
 const userInput = document.querySelector('#user-input')
 const randomQuoteButton = document.querySelector('#randomQuoteButton')
-//................
 
 //FUNCTIONS
 
-//Function to handle fetch
+//Function to handle user inputed quote fetch
 let userQuoteFetch = () => {
 	// console.log('im in user search')
 	fetch (animeListUrl + userInput.value)
@@ -18,31 +17,38 @@ let userQuoteFetch = () => {
 			alert('Sorry No Quotes!!')
 		})
 }
-
+//Function to handle random quote fetch
 let randomQuoteFetch = () => {
 	return fetch (randomUrl)
 		.then(response => response.json())
 		.then(randomQuoteObject => randomQuoteSearch(randomQuoteObject))
 }
 
-//Funtion for searching by name and character (seperate?)
+//Funtion for searching quotes by anime title
 let nameCharacterSearch = animeListArr => {
-	for (const animeObjList of animeListArr) {
-		const {anime, character, quote} = animeObjList;
-		console.log(animeObjList)
-		console.log(`Here is a quote from ${character.toUpperCase()} of ${anime.toUpperCase()}: "${quote}"`)		
-	}
+	// //FEATURE 1 (Need to Finish)
+	// //ask user to pick random number between 1-10???????
+	// //then place in animeListArr[....]
+	// const userNumberInput = 0 //ask user for number 1-10
+	// const {anime, character, quote} = animeListArr[userNumberInput];
+	// console.log(anime)
+	// console.log(character)
+	// console.log(quote)
+
+	// //FEATURE 2
+	// //prints all 10 of the quotes
+	// for (const animeObjList of animeListArr) {
+	// 	const {anime, character, quote} = animeObjList;
+	// 	console.log(animeObjList)
+	// 	console.log(`Here is a quote from the character ${character.toUpperCase()} of the anime ${anime.toUpperCase()}: "${quote}"`)		
+	// }
 }
 
-//Function for searching random
+//Function for searching random quotes
 let randomQuoteSearch = randomQuoteObject => {
 	console.log(randomQuoteObject)
-	for (const animeObjList of animeListArr) {
-		const {anime, character, quote} = animeObjList;
-		console.log(animeObjList)
-		console.log(`Here is a quote from ${character.toUpperCase()} of ${anime.toUpperCase()}: "${quote}"`)		
-	}
-
+	const {anime, character, quote} = randomQuoteObject;
+	console.log(`Here is a quote from the character ${character.toUpperCase()} of the anime ${anime.toUpperCase()}: "${quote}"`)
 }
 
 //EVENT LISTENERS
